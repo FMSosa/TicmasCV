ca63d8c752adb28c564d1b3b0e2b6753995e51b5
F.M. Sosa <123365736+FMSosa@users.noreply.github.com>
2023-02-16 17:05:41 -0300

    Add files via upload

---
+++ TP1-CV/js/funciones.js
@@ -0,0 +1,8 @@
+
+function cambiarTexto() {
+    document.getElementById("textoDeBienvenida").innerHTML="Que tenga un Buen Día";
+}
+
+var button = document.getElementById("boton");
+button.onclick = cambiarTexto;
+
